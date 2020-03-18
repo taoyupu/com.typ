4eb2a480bc3624411f0687719780b0938da11795
陶玉璞 <1094991018@qq.com>
2020-03-18 11:03:20 +0800

    提交测试远程配置所需文件&&删除多余文件

--- account/src/main/java/com/typ/account/controller/ClientTest.java
+++ account/src/main/java/com/typ/account/controller/ClientTest.java
@@ -1,5 +1,7 @@
 package com.typ.account.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RefreshScope
 public class ClientTest {
+
+    @Value("${env}")
+    public String env;
+
+    @GetMapping("/env")
+    public String getEnv() {
+        return env;
+    }
 
     @GetMapping("/getMessage")
     public String getMessage() {
